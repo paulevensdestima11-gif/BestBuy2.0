499aee94a06db9b379d821f9073e341864cebaf6
Paul <paulevensdestima11@gmail.com>
2026-05-28 01:05:03 +0200

    adding the nonstocked and limited products

--- products.py
+++ products.py
@@ -43,6 +43,36 @@ class Product:
         return self._price * quantity
 
 
+class NonStockedProduct(Product):
+    def __init__(self, name, price):
+        super().__init__(name, price, quantity=0)
+
+    def set_quantity(self, quantity):
+        pass
+
+    def show(self):
+        return f"{self._name}, {self._price} is not stocked"
+
+
+class LimitedProduct(Product):
+    def __init__(self, name, price, quantity, maximum):
+        super().__init__(name, price, quantity)
+        self._maximum = maximum
+
+    def buy(self, quantity):
+        if quantity > self._maximum:
+            raise ValueError(f"Cannot buy more than {self._maximum} of {self._name}")
+        return super().buy(quantity)
+
+    def show(self):
+        return (f"{self._name}, Price: {self._price}, "
+                f"Quantity: {self._quantity}, Maximum per order: {self._maximum}")
+
+
+
+
+
+
 
 
 
